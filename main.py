0992f1964547cd00fd2f738ffc7dbc3cd95886c3
Noam Rathaus <rathaus@gmail.com>
2024-06-02 12:52:12 +0900

    Support external user agent strings, generalize read file

--- main.py
+++ main.py
@@ -5,13 +5,25 @@ import random
 import requests
 
 
-def read_headers_from_file() -> list:
-    """Read the 'headers.txt' file and return it as a list"""
-    data = ""
-    with open(file="headers.txt", mode="r", encoding="latin1") as file_handle:
-        data = file_handle.read()
+def read_from_file(filename: str) -> list:
+    """Read file and return it as a list"""
+    lines = []
+    with open(file=filename, mode="r", encoding="latin1") as file_handle:
+        while True:
+            line = file_handle.readline()
+            if not line:
+                break
 
-    return data.split(sep="\n")
+            if line == "":
+                continue
+
+            if line.startswith("#"):
+                continue
+
+            line = line.replace("\n", "")
+            lines.append(line)
+
+    return lines
 
 
 def generate_cache_buster() -> str:
@@ -23,7 +35,9 @@ def generate_cache_buster() -> str:
     return string
 
 
-def make_request(base_url: str, incoming_header: str) -> tuple[object, object]:
+def make_request(
+    base_url: str, incoming_header: str, user_agents: list
+) -> tuple[object, object]:
     """Send a request to a server with a given url and custom header
     Return the response (HTTPResponse obj) and exception if occurred
     """
@@ -34,6 +48,10 @@ def make_request(base_url: str, incoming_header: str) -> tuple[object, object]:
         return (None, None)
 
     headers = {parts[0]: parts[1]}
+
+    # Pick a random user agent - instead of the python-request
+    user_agent = random.choice(user_agents)
+    headers["User-Agent"] = user_agent
 
     exception = None
     try:
@@ -59,32 +77,40 @@ def print_results(results):
             average_content_lengths[f'{result["content_length"]}'] = 0
         average_content_lengths[f'{result["content_length"]}'] += 1
 
-
     most_common_content_length = None
-    for (content_name, content_count) in average_content_lengths.items():
+    for content_name, content_count in average_content_lengths.items():
         if most_common_content_length is None:
             most_common_content_length = {"name": content_name, "count": content_count}
             continue
 
-        if most_common_content_length['count'] < content_count:
+        if most_common_content_length["count"] < content_count:
             most_common_content_length = {"name": content_name, "count": content_count}
 
-    print(f"Most common Content-Length: {most_common_content_length['name']}, hit count: {most_common_content_length['count']}")
+    print(
+        f"Most common Content-Length: {most_common_content_length['name']}, "
+        f"hit count: {most_common_content_length['count']}"
+    )
     print("Printing exceptions to the norm:")
+    abnormal_count = 0
     for result in results:
-        if (result['exception'] is None and
-            result['status_code'] == 200 and
-            result['content_length'] == int(most_common_content_length["name"])
-            ):
+        if (
+            result["exception"] is None
+            and result["status_code"] == 200
+            and result["content_length"] == int(most_common_content_length["name"])
+        ):
             continue
 
+        abnormal_count += 1
         print(
-            f"[{result['status_code']}] [CL:{result['content_length']}] "
+            f"  [{result['status_code']}] [CL:{result['content_length']}] "
             f"[E:{result['exception']}] [{result['header']}]"
         )
 
+    if abnormal_count == 0:
+        print("  No abnormal responses found")
 
-def request_worker(results, q, base_url):
+
+def request_worker(results, q, base_url, user_agents):
     """request_worker"""
     while True:
         try:
@@ -92,9 +118,12 @@ def request_worker(results, q, base_url):
         except:
             break
 
-        print(f"Working on '{header}' ({q.qsize()})                                    \r", end="")
+        print(
+            f"Working on '{header}' (left: {q.qsize()})                                    \r",
+            end="",
+        )
 
-        (response, exception) = make_request(base_url, header)
+        (response, exception) = make_request(base_url, header, user_agents)
         if response is None and exception is None:
             continue
 
@@ -127,7 +156,9 @@ def main():
     base_url = "http://zero.webappsecurity.com/"
     thread_count = 30
 
-    headers = read_headers_from_file()
+    headers = read_from_file("headers.txt")
+    user_agents = read_from_file("useragents.txt")
+    # headers = headers[-2000:]
     headers.sort()
     q = queue.Queue()
     for header in headers:
@@ -137,7 +168,7 @@ def main():
 
     for _ in range(thread_count):
         threading.Thread(
-            target=request_worker, args=[results, q, base_url], daemon=True
+            target=request_worker, args=[results, q, base_url, user_agents], daemon=True
         ).start()
 
     q.join()
